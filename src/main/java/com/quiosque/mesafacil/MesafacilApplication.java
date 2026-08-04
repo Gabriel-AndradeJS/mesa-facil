ac04cac17b6b8fa7da0c0a403d021c6b798b1b23
gabriel <gabriel.andrade@azultecnologia.com>
2026-08-04 11:00:00 -0300

    first commit

---
+++ src/main/java/com/quiosque/mesafacil/MesafacilApplication.java
@@ -0,0 +1,13 @@
+package com.quiosque.mesafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesafacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesafacilApplication.class, args);
+	}
+
+}
