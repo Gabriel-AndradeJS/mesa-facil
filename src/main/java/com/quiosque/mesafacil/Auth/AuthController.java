88544b381db96ca2fde9af9357637896700aa3a7
gabriel <gabriel.andrade@azultecnologia.com>
2026-08-10 15:30:56 -0300

    Add AuthController for authentication endpoints

---
+++ src/main/java/com/quiosque/mesafacil/Auth/AuthController.java
@@ -0,0 +1,15 @@
+package com.quiosque.mesafacil.Auth;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/auth")
+public class AuthController {
+
+    @GetMapping
+    public String login(){
+        return "Login";
+    }
+}
