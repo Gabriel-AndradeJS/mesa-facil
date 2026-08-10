8b5ee951ac73795d4b4a4904e00550e9116cc1e6
gabriel <gabriel.andrade@azultecnologia.com>
2026-08-10 15:31:25 -0300

    Implement UserDetails in UserEntity for Spring Security integration

--- src/main/java/com/quiosque/mesafacil/User/Entity/UserEntity.java
+++ src/main/java/com/quiosque/mesafacil/User/Entity/UserEntity.java
@@ -3,12 +3,17 @@ package com.quiosque.mesafacil.User.Entity;
 import com.quiosque.mesafacil.User.enums.UserRole;
 import jakarta.persistence.*;
 import org.hibernate.annotations.*;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,4 +37,39 @@ public class UserEntity {
 
     @UpdateTimestamp
     private LocalDateTime updateAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
