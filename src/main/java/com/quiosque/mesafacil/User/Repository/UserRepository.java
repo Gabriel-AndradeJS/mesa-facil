48c3d926ba4e55bd170d9857b63b52319edc210d
gabriel <gabriel.andrade@azultecnologia.com>
2026-08-11 10:37:43 -0300

    Add findByEmail method to UserRepository for email-based user retrieval

--- src/main/java/com/quiosque/mesafacil/User/Repository/UserRepository.java
+++ src/main/java/com/quiosque/mesafacil/User/Repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.quiosque.mesafacil.User.Repository;
 import com.quiosque.mesafacil.User.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
 }
