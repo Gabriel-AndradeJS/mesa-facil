2a6a7bbe488168c326c1206ce9b8f2720f5c4cf8
gabriel <gabriel.andrade@azultecnologia.com>
2026-08-11 10:37:54 -0300

    Refactor UserService to enhance user creation and add user retrieval functionality

--- src/main/java/com/quiosque/mesafacil/User/Service/UserService.java
+++ src/main/java/com/quiosque/mesafacil/User/Service/UserService.java
@@ -2,10 +2,15 @@ package com.quiosque.mesafacil.User.Service;
 
 import com.quiosque.mesafacil.User.DTOs.CreateUserDTO;
 import com.quiosque.mesafacil.User.DTOs.ResponseUserDTO;
+import com.quiosque.mesafacil.User.Entity.UserEntity;
 import com.quiosque.mesafacil.User.Mapper.UserMapper;
+import com.quiosque.mesafacil.User.Repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Service
@@ -13,9 +18,23 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private UserMapper mapper;
+    private final UserRepository userRepository;
 
-    public ResponseUserDTO createUser(CreateUserDTO dto){
-        log.info("Creating user with Senha: {}", dto.getPassword());
-        return mapper.createToResponse(dto);
+    public ResponseEntity<ResponseUserDTO> createUser(CreateUserDTO dto){
+        UserEntity user = UserEntity.builder()
+                .name(dto.getName())
+                .email(dto.getEmail())
+                .password(dto.getPassword())
+                .role(dto.getRole())
+                .build();
+        UserEntity savedUser = userRepository.save(user);
+        ResponseUserDTO responseUserDTO = mapper.EntityToResponse(savedUser);
+        return ResponseEntity.ok(responseUserDTO);
+    }
+
+    public List<ResponseUserDTO> getUsers(){
+        return userRepository.findAll().stream()
+                .map(mapper::EntityToResponse)
+                .toList();
     }
 }
