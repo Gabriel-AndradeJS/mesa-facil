9672d39c66bb159d276823eeedf785b79b9608ef
gabriel <gabriel.andrade@azultecnologia.com>
2026-08-11 10:37:22 -0300

    Refactor UserController to add user retrieval endpoint and improve user creation response

--- src/main/java/com/quiosque/mesafacil/User/Controller/UserController.java
+++ src/main/java/com/quiosque/mesafacil/User/Controller/UserController.java
@@ -3,11 +3,12 @@ package com.quiosque.mesafacil.User.Controller;
 import com.quiosque.mesafacil.User.DTOs.CreateUserDTO;
 import com.quiosque.mesafacil.User.DTOs.ResponseUserDTO;
 import com.quiosque.mesafacil.User.Service.UserService;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/user")
@@ -21,16 +22,16 @@ public class UserController {
         this.passwordEncoder = passwordEncoder;
     }
 
-    @GetMapping
-    public String ObterUser(){
-        return "Teste ok!";
-    }
-
 
     @PostMapping
-    public ResponseUserDTO createUser(@RequestBody CreateUserDTO dto){
+    public ResponseEntity<ResponseUserDTO> createUser(@Valid @RequestBody CreateUserDTO dto){
         dto.setPassword(passwordEncoder.encode(dto.getPassword()));
         return this.userService.createUser(dto);
+    }
+
+    @GetMapping
+    public List<ResponseUserDTO> getUsers(){
+        return this.userService.getUsers();
     }
 
 }
