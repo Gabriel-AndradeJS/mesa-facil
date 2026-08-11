bc8e79df3fe2e98b1a8c33678e7ea305fdff19e3
gabriel <gabriel.andrade@azultecnologia.com>
2026-08-11 10:36:32 -0300

    Implement login endpoint in AuthController with LoginService integration

--- src/main/java/com/quiosque/mesafacil/Auth/AuthController.java
+++ src/main/java/com/quiosque/mesafacil/Auth/AuthController.java
@@ -1,15 +1,21 @@
 package com.quiosque.mesafacil.Auth;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.quiosque.mesafacil.Auth.DTO.LoginDTO;
+import com.quiosque.mesafacil.Auth.DTO.ResponseLogin;
+import com.quiosque.mesafacil.Auth.Service.LoginService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/auth")
+@AllArgsConstructor
 public class AuthController {
 
-    @GetMapping
-    public String login(){
-        return "Login";
+    private final LoginService loginService;
+
+    @PostMapping
+    public ResponseLogin login(@RequestBody LoginDTO loginDTO) {
+        return loginService.login(loginDTO);
     }
 }
