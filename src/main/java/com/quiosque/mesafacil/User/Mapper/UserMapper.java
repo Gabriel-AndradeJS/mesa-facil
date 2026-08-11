baea27ce1b55a671a1c1a9330175f8fc59d123e6
gabriel <gabriel.andrade@azultecnologia.com>
2026-08-11 10:37:37 -0300

    Add UserMapper methods for entity conversion and response mapping

--- src/main/java/com/quiosque/mesafacil/User/Mapper/UserMapper.java
+++ src/main/java/com/quiosque/mesafacil/User/Mapper/UserMapper.java
@@ -2,10 +2,15 @@ package com.quiosque.mesafacil.User.Mapper;
 
 import com.quiosque.mesafacil.User.DTOs.CreateUserDTO;
 import com.quiosque.mesafacil.User.DTOs.ResponseUserDTO;
+import com.quiosque.mesafacil.User.Entity.UserEntity;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
      ResponseUserDTO createToResponse(CreateUserDTO user);
+
+     UserEntity createToEntity(CreateUserDTO user);
+
+     ResponseUserDTO EntityToResponse(UserEntity user);
 }
