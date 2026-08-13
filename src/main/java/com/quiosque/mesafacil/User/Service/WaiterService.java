094a91d64927217ab754faed5b3ae94c9fb79fdb
gabriel <gabriel.andrade@azultecnologia.com>
2026-08-13 14:56:11 -0300

    Add WaiterService for creating waiters with admin authorization

---
+++ src/main/java/com/quiosque/mesafacil/User/Service/WaiterService.java
@@ -0,0 +1,69 @@
+package com.quiosque.mesafacil.User.Service;
+
+import com.quiosque.mesafacil.Config.JwtService;
+import com.quiosque.mesafacil.User.DTOs.CreateWaiterDTO;
+import com.quiosque.mesafacil.User.DTOs.WaiterDTO;
+import com.quiosque.mesafacil.User.Entity.UserEntity;
+import com.quiosque.mesafacil.User.Entity.WaiterEntity;
+import com.quiosque.mesafacil.User.Mapper.UserMapper;
+import com.quiosque.mesafacil.User.Repository.UserRepository;
+import com.quiosque.mesafacil.User.Repository.WaiterRepository;
+import com.quiosque.mesafacil.User.enums.UserRole;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WaiterService {
+
+    private final UserRepository userRepository;
+    private final WaiterRepository waiterRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserMapper userMapper;
+    private final JwtService jwtService;
+
+    public WaiterService(
+            UserRepository userRepository,
+            WaiterRepository waiterRepository,
+            PasswordEncoder passwordEncoder,
+            UserMapper userMapper,
+            JwtService jwtService
+    ) {
+        this.userRepository = userRepository;
+        this.waiterRepository = waiterRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.userMapper = userMapper;
+        this.jwtService = jwtService;
+    }
+
+    public ResponseEntity<WaiterDTO> createWaiter(CreateWaiterDTO dto, String admin) {
+        String token = admin.replace("Bearer ", "");
+        String emailAdmin = jwtService.extractSubject(token);
+
+        UserEntity adminUser = userRepository.findByEmail(emailAdmin)
+                .orElseThrow(() -> new RuntimeException("Admin not found"));
+
+        UserEntity waiterUser = new UserEntity();
+
+        waiterUser.setName(dto.getName());
+        waiterUser.setEmail(dto.getEmail());
+        waiterUser.setPassword(
+                passwordEncoder.encode(dto.getPassword())
+        );
+        waiterUser.setRole(UserRole.WAITER);
+
+        userRepository.save(waiterUser);
+
+        WaiterEntity waiter = new WaiterEntity();
+
+        waiter.setName(dto.getName());
+        waiter.setUser(waiterUser);
+        waiter.setAdmin(adminUser);
+
+        WaiterEntity savedWaiter = waiterRepository.save(waiter);
+
+        WaiterDTO waiterDTO = userMapper.WaiterEntityToWaiter(savedWaiter);
+        return ResponseEntity.status(HttpStatus.CREATED).body(waiterDTO);
+    }
+}
