13988fddbf22c4e73d8564140fee56975d3080e6
gabriel <gabriel.andrade@azultecnologia.com>
2026-08-13 14:56:05 -0300

    Add WaiterRepository interface for accessing waiter data by admin ID

---
+++ src/main/java/com/quiosque/mesafacil/User/Repository/WaiterRepository.java
@@ -0,0 +1,10 @@
+package com.quiosque.mesafacil.User.Repository;
+
+import com.quiosque.mesafacil.User.Entity.WaiterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface WaiterRepository extends JpaRepository<WaiterEntity, Long> {
+    List<WaiterEntity> findAllByAdminId(Long adminId);
+}
