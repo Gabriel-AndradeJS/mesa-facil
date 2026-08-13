f0708e153be1796521aebddac4a906f21f6d793c
gabriel <gabriel.andrade@azultecnologia.com>
2026-08-13 14:55:19 -0300

    Add AuthoritiesDTO class with Lombok annotations for data transfer

---
+++ src/main/java/com/quiosque/mesafacil/User/DTOs/AuthoritiesDTO.java
@@ -0,0 +1,15 @@
+package com.quiosque.mesafacil.User.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AuthoritiesDTO {
+
+    private String authority;
+}
