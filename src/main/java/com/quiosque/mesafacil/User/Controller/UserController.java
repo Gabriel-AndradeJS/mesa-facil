627af51ca401d47a8739bd2b099820231d2998bc
gabriel <gabriel.andrade@azultecnologia.com>
2026-08-13 14:55:34 -0300

    Add WaiterService integration to UserController for managing waiters

--- src/main/java/com/quiosque/mesafacil/User/Controller/UserController.java
+++ src/main/java/com/quiosque/mesafacil/User/Controller/UserController.java
@@ -1,8 +1,11 @@
 package com.quiosque.mesafacil.User.Controller;
 
 import com.quiosque.mesafacil.User.DTOs.CreateUserDTO;
+import com.quiosque.mesafacil.User.DTOs.CreateWaiterDTO;
 import com.quiosque.mesafacil.User.DTOs.ResponseUserDTO;
+import com.quiosque.mesafacil.User.DTOs.WaiterDTO;
 import com.quiosque.mesafacil.User.Service.UserService;
+import com.quiosque.mesafacil.User.Service.WaiterService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,12 +17,14 @@ import java.util.List;
 @RequestMapping("api/user")
 public class UserController {
 
-    private UserService userService;
-    private PasswordEncoder passwordEncoder;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final WaiterService waiterService;
 
-    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
+    public UserController(UserService userService, PasswordEncoder passwordEncoder, WaiterService waiterService) {
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
+        this.waiterService = waiterService;
     }
 
 
@@ -34,4 +39,18 @@ public class UserController {
         return this.userService.getUsers();
     }
 
+    @GetMapping("waiters")
+    public List<WaiterDTO> getWaiters(@RequestHeader("Authorization") String token) {
+        return this.userService.getWaiters(token);
+    }
+
+    @GetMapping("roles")
+    public List<WaiterDTO> getRoles() {
+        return this.userService.getRoles();
+    }
+
+    @PostMapping("waiters")
+    public ResponseEntity<WaiterDTO> createWaiter(@Valid @RequestBody CreateWaiterDTO dto, @RequestHeader("Authorization") String token) {
+        return this.waiterService.createWaiter(dto, token);
+    }
 }
