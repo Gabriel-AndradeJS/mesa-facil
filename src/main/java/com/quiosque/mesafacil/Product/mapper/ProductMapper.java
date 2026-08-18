08fea6a7f4b7d8b4b20c759fb6b4927220d6ab65
gabriel <gabriel.andrade@azultecnologia.com>
2026-08-17 22:13:13 -0300

    Add ProductMapper for mapping between ProductEntity and ResponseProductDTO

---
+++ src/main/java/com/quiosque/mesafacil/Product/mapper/ProductMapper.java
@@ -0,0 +1,9 @@
+package com.quiosque.mesafacil.Product.mapper;
+
+import com.quiosque.mesafacil.Product.DTOs.ResponseProductDTO;
+import com.quiosque.mesafacil.Product.Entity.ProductEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public class ProductMapper {
+}
