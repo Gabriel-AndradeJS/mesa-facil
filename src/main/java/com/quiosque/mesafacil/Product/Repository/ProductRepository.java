c329c98665427478f9b7676ffb61f81d611f4a12
gabriel <gabriel.andrade@azultecnologia.com>
2026-08-18 10:38:36 -0300

    Add findAllProduct method to ProductRepository for retrieving products by admin ID

--- src/main/java/com/quiosque/mesafacil/Product/Repository/ProductRepository.java
+++ src/main/java/com/quiosque/mesafacil/Product/Repository/ProductRepository.java
@@ -2,6 +2,18 @@ package com.quiosque.mesafacil.Product.Repository;
 
 import com.quiosque.mesafacil.Product.Entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+    @Query("""
+    SELECT p FROM ProductEntity p
+    WHERE p.admin.id = :id
+""")
+    List<ProductEntity> findAllProduct(
+             Long id
+    );
 }
