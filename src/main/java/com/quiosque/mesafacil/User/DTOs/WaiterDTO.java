b5f8dc275743b251c03b078fda24cc047b57d963
gabriel <gabriel.andrade@azultecnologia.com>
2026-08-17 22:13:46 -0300

    Add toString method to WaiterDTO for improved string representation

--- src/main/java/com/quiosque/mesafacil/User/DTOs/WaiterDTO.java
+++ src/main/java/com/quiosque/mesafacil/User/DTOs/WaiterDTO.java
@@ -45,4 +45,22 @@ public class WaiterDTO {
 
     private LocalDateTime createdAt;
 
+
+    public String toString() {
+        return "WaiterDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", role=" + role +
+                ", userId=" + userId +
+                ", adminId=" + adminId +
+                ", accountNonExpired=" + accountNonExpired +
+                ", accountNonLocked=" + accountNonLocked +
+                ", credentialsNonExpired=" + credentialsNonExpired +
+                ", enabled=" + enabled +
+                ", authorities=" + authorities +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+
 }
