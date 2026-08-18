4e573a671c0e4c5c0889ea54e3daad8b42384943
gabriel <gabriel.andrade@azultecnologia.com>
2026-08-18 10:38:31 -0300

    Add productToResponse method in ProductMapper for mapping ProductEntity to ResponseProductDTO

--- src/main/java/com/quiosque/mesafacil/Product/mapper/ProductMapper.java
+++ src/main/java/com/quiosque/mesafacil/Product/mapper/ProductMapper.java
@@ -6,4 +6,25 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public class ProductMapper {
+
+    public ResponseProductDTO productToResponse(ProductEntity product) {
+        ResponseProductDTO response = new ResponseProductDTO();
+
+        response.setId(product.getId());
+        response.setName(product.getName());
+        response.setDescription(product.getDescription());
+        response.setPrice(product.getPrice());
+        response.setQuantity(product.getQuantity());
+        response.setStatus(product.getStatus());
+
+        if (product.getCreatedBy() != null) {
+            response.setWaiterName(product.getCreatedBy().getName());
+            response.setCreatedById(product.getCreatedBy().getId());
+        } else if (product.getAdmin() != null) {
+            response.setWaiterName(product.getAdmin().getName());
+            response.setCreatedById(product.getAdmin().getId());
+        }
+
+        return response;
+    }
 }
