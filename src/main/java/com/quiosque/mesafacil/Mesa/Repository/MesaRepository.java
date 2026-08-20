a69b7f2035ca33f8b2be83454b5c36b8234aabaf
gabriel <gabriel.andrade@azultecnologia.com>
2026-08-20 19:54:09 -0300

    Add MesaRepository interface for database operations on MesaEntity

---
+++ src/main/java/com/quiosque/mesafacil/Mesa/Repository/MesaRepository.java
@@ -0,0 +1,7 @@
+package com.quiosque.mesafacil.Mesa.Repository;
+
+import com.quiosque.mesafacil.Mesa.Entity.MesaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MesaRepository extends JpaRepository<MesaEntity, Long> {
+}
