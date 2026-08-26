09b46750d866c0fe40392cade98652fbc88f17ff
gabriel <gabriel.andrade@azultecnologia.com>
2026-08-26 19:57:27 -0300

    Add method to MesaRepository for finding MesaEntity by number

--- src/main/java/com/quiosque/mesafacil/mesa/repository/MesaRepository.java
+++ src/main/java/com/quiosque/mesafacil/mesa/repository/MesaRepository.java
@@ -4,4 +4,6 @@ import com.quiosque.mesafacil.mesa.entity.MesaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MesaRepository extends JpaRepository<MesaEntity, Long> {
+
+    MesaEntity findByNumber(Integer number);
 }
