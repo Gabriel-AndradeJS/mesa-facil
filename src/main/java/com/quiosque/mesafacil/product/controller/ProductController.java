cc148963f7cba9a89bd6bb90d596adc38fe32c3c
gabriel <gabriel.andrade@azultecnologia.com>
2026-08-30 14:52:21 -0300

    Refactor ProductController to use CurrentSecurityContext for user authentication in getAllProducts method

--- src/main/java/com/quiosque/mesafacil/product/controller/ProductController.java
+++ src/main/java/com/quiosque/mesafacil/product/controller/ProductController.java
@@ -3,8 +3,10 @@ package com.quiosque.mesafacil.product.controller;
 import com.quiosque.mesafacil.product.dto.CreateProductDTO;
 import com.quiosque.mesafacil.product.dto.ResponseProductDTO;
 import com.quiosque.mesafacil.product.service.ProductService;
+import com.quiosque.mesafacil.user.entity.UserEntity;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,7 +24,9 @@ public class ProductController {
     }
 
     @GetMapping
-    public List<ResponseProductDTO> getAllProducts(@RequestHeader("Authorization") String token) {
-        return productService.getAllProducts(token);
+    public List<ResponseProductDTO> getAllProducts(
+            @CurrentSecurityContext(expression = "authentication.principal") UserEntity user
+    ) {
+        return productService.getAllProducts(user.getId());
     }
 }
