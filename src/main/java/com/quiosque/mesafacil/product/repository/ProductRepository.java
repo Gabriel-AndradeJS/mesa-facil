5dcd7334739bcc8bbe87486b6c99f30b8315fa36
gabriel <gabriel.andrade@azultecnologia.com>
2026-09-03 13:38:21 -0300

    Refactor ProductRepository to update findAllByMesaId method and add deleteAllByAdminId method

--- src/main/java/com/quiosque/mesafacil/product/repository/ProductRepository.java
+++ src/main/java/com/quiosque/mesafacil/product/repository/ProductRepository.java
@@ -3,7 +3,6 @@ package com.quiosque.mesafacil.product.repository;
 import com.quiosque.mesafacil.product.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -17,10 +16,7 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
              Long id
     );
 
-    @Query("""
-    SELECT p
-    FROM ProductEntity p
-    WHERE p.mesaId.id = :mesaId
-""")
-    List<ProductEntity> findAllByMesaId(@Param("mesaId") Long mesaId);
+    List<ProductEntity> findAllByMesaIdIdAndAdminId(Long mesaId, Long adminId);
+
+    void deleteAllByAdminId(Long adminId);
 }
