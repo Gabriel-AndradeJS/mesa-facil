7d113fe000e71d571d935b58783aa441f3d55821
gabriel <gabriel.andrade@azultecnologia.com>
2026-09-03 11:19:09 -0300

    Add findById method to TableRepository for optional table retrieval

--- src/main/java/com/quiosque/mesafacil/table/repository/TableRepository.java
+++ src/main/java/com/quiosque/mesafacil/table/repository/TableRepository.java
@@ -3,7 +3,11 @@ package com.quiosque.mesafacil.table.repository;
 import com.quiosque.mesafacil.table.entity.TableEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TableRepository extends JpaRepository<TableEntity, Long> {
 
     TableEntity findByNumber(Integer number);
+
+    Optional<TableEntity> findById(Long id);
 }
