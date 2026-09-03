e52c93143535b01bbbb5a55b3d601a0a21af9c2b
gabriel <gabriel.andrade@azultecnologia.com>
2026-09-03 11:18:22 -0300

    Add deleteProduct method to ProductController for bulk deletion of products

--- src/main/java/com/quiosque/mesafacil/product/controller/ProductController.java
+++ src/main/java/com/quiosque/mesafacil/product/controller/ProductController.java
@@ -29,4 +29,9 @@ public class ProductController {
     ) {
         return productService.getAllProducts(user.getId());
     }
+
+    @DeleteMapping
+    public void deleteProduct() {
+        productService.deleteAll();
+    }
 }
